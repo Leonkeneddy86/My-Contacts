c45a931fa6265f2b688bdc2bcc5b9785c80573de
leonkenedy86 <jonathan19.jtv@gmail.com>
2025-02-08 21:28:39 +0100

    feat:Model

--- app/Models/Contacts.php
+++ app/Models/Contacts.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contacts extends Model
 {
-    //
+    protected $table = 'contacts';
+    protected $fillable = ['name', 'phone_id', 'phone_number'];
 }
