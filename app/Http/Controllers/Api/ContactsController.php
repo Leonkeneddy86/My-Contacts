8c57f513d370b0fcdd9190a0a7630dc4d41f9e79
leonkenedy86 <jonathan19.jtv@gmail.com>
2025-02-08 21:41:43 +0100

    fix: controllers

--- app/Http/Controllers/Api/ContactsController.php
+++ app/Http/Controllers/Api/ContactsController.php
@@ -1,65 +1,29 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
-use App\Models\Contacts;
 use Illuminate\Http\Request;
 
-class ContactsController extends Controller
+class ContactsController
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Contacts $contacts)
+    public function show($id)
     {
-        //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Contacts $contacts)
+    public function update(Request $request, $id)
     {
-        //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Contacts $contacts)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Contacts $contacts)
-    {
-        //
     }
 }
