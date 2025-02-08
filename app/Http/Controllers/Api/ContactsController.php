a7dc7f1ef18a4c70941141e292074ba3b7fd07b9
leonkenedy86 <jonathan19.jtv@gmail.com>
2025-02-08 21:33:18 +0100

    feat: controllers

---
+++ app/Http/Controllers/Api/ContactsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+class ContactsController
+{
+    public function index()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
